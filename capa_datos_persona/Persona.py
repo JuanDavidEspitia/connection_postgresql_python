bd78aebfef5e1424b13f64b6b99444d9ded73401
JuanEspitia14 <juandavid.espitia@mercadolibre.com.co>
2022-11-20 17:03:59 -0500

    create class persona and class Logging

---
+++ capa_datos_persona/Persona.py
@@ -0,0 +1,63 @@
+from logger_base import log
+
+class Persona:
+
+    def __init__(self, id_persona=None, nombre=None, apellido=None, email=None):
+        self._id_persona = id_persona
+        self._nombre = nombre
+        self._apellido = apellido
+        self._email = email
+
+    def __str__(self):
+        return f'''
+        ID Persona: {self._id_persona}, Nombre: {self._nombre},
+        Apellido: {self._apellido}, Email: {self._email}
+        '''
+
+    """
+    Metodos GET y SET
+    """
+    @property
+    def id_persona(self):
+        return self._id_persona
+
+    @id_persona.setter
+    def id_persona(self, id_persona):
+        self._id_persona = id_persona
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+    @property
+    def apellido(self):
+        return self._apellido
+
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+
+if __name__ == '__main__':
+    persona1 = Persona(1, 'Juan', 'Perez', 'juan@gmail.com')
+    log.debug(persona1)
+
+    # Simular un insert
+    persona2 = Persona(id_persona=2, nombre='Juan', apellido='Perez', email='juan@gmail.com')
+    log.debug(persona2)
+
+    # Simular un delete
+    persona3 = Persona(id_persona=3)
+    log.debug(persona3)
--- capa_datos_persona/logger_base.py
+++ capa_datos_persona/logger_base.py
@@ -1,11 +1,14 @@
 import logging as log
 
 # si lo cambiamos a INFO, solo muestra de INFO en adelante, es por jerarquia de Logs
-log.basicConfig(level=log.DEBUG)
-
-
-
-
+# Con los sigueintes parametros ajustamos el mensaje de como queremos que salga en consola o arhcivo
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M%:%S %p',
+                handlers=[
+                    log.FileHandler('capa_datos.log'),
+                    log.StreamHandler()
+                ])
 
 if __name__ == '__main__':
     log.debug('Mensaje a nivel de Debug')
